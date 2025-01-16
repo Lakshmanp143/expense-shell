8c71492ad65a0fd5c365d7fea7510845b37e48a6
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-16 12:16:12 +0530

    shell script

---
+++ mysql.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+USERID=$(id -u)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGS_FOLDER="/var/log/expense-logs"
+LOGS_FILE=$(echo $0 | cut -d "." -f1)
+lOGS_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: $B You must be root user to execute this script $N "
+        exit 1
+    fi
+}
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at:: $TIMESTAMP" &>>$lOGS_FILE_NAME
+
+CHECK_ROOT
+ 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ...... $R FAILURE $N "
+        exit 1
+    else
+        echo -e "$2 ...... $G SUCCESS $N "
+    fi
+}
+
+dnf list installed | grep mysql-server  &>>$lOGS_FILE_NAME
+if [ $? -ne 0 ]
+then    
+    echo -e "Mysql-server not installed, $Y INSTALLING ..WAIT $N "
+
+    dnf install mysql-server &>>$lOGS_FILE_NAME
+    VALIDATE $?  "installing mysql-server"
+
+    systemctl start mysqld  &>>$lOGS_FILE_NAME
+    VALIDATE $? "Enabling mysql-server"
+
+    systemctl start mysqld  &>>$lOGS_FILE_NAME
+    VALIDATE $? "Starting mysql-server"
+
+    mysql -h mysql.lakshman.site -u root -pExpenseApp@1 -e 'show databases;' &>>$lOGS_FILE_NAME
+    if [ $? -ne 0 ]
+    then
+        echo -e "mysql password not setup, $B setting up ..WAIT!!! $N "
+
+        mysql_secure_installtion --set-root-pass ExpenseApp@1  &>>$lOGS_FILE_NAME
+        VALIDATE $? "Setting root password"
+    else   
+        echo -e "Mysql password $Y ..ALREADY SETUP ..SKIPPING $N "
+    fi
+else    
+    echo -e "Mysql-server is $Y ALREADY INSTALLED ..SKIPPING $N "
+fi
