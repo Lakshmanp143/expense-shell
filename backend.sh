3995fe06687d7c03b3fd9ea896f9b218c0260198
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-16 13:44:35 +0530

    shell script

--- backend.sh
+++ backend.sh
@@ -7,7 +7,7 @@ B="\e[34m"
 N="\e[0m"
 USERID=$(id -u)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOGS_FOLDER="/var/log/expense-logs"
+LOGS_FOLDER="/var/log/expense1-logs"
 LOGS_FILE=$(echo $0 | cut -d "." -f1)
 lOGS_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
 
